933f310503d00a3b1aafeafc260092e9d02b8fb7
Tim <intezya@gmail.com>
2025-06-25 20:32:56 +0400

    Initial commit

---
+++ src/main/java/com/intezya/netherburns/mixin/PlayerManagerMixin.java
@@ -0,0 +1,26 @@
+package com.intezya.netherburns.mixin;
+
+import net.minecraft.network.ClientConnection;
+import net.minecraft.server.PlayerManager;
+import net.minecraft.server.network.ConnectedClientData;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerManager.class)
+public class PlayerManagerMixin {
+    @Inject(method = "onPlayerConnect", at = @At("TAIL"))
+    private void onPlayerConnect(
+            ClientConnection connection,
+            ServerPlayerEntity player,
+            ConnectedClientData clientData,
+            CallbackInfo ci
+    ) {
+        if (player.getWorld().getRegistryKey() == World.NETHER) {
+            player.setOnFireFor(Integer.MAX_VALUE);
+        }
+    }
+}
---
+++ src/main/java/com/intezya/netherburns/mixin/ServerPlayerEntityMixin.java
@@ -0,0 +1,31 @@
+package com.intezya.netherburns.mixin;
+
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ServerPlayerEntity.class)
+public class ServerPlayerEntityMixin {
+    @Inject(method = "worldChanged", at = @At("TAIL"))
+    private void onWorldChange(ServerWorld origin, CallbackInfo ci) {
+        ServerPlayerEntity player = (ServerPlayerEntity) (Object) this;
+
+        final RegistryKey<World> originRegistryKey = origin.getRegistryKey();
+        final RegistryKey<World> playerRegistryKey = player.getWorld().getRegistryKey();
+
+        if (playerRegistryKey == World.NETHER) {
+            player.setOnFireFor(Integer.MAX_VALUE);
+        } else if (originRegistryKey == World.NETHER) {
+            if (player.isOnFire()) {
+                player.extinguish();
+            }
+
+            player.setOnFireFor(5);
+        }
+    }
+}
